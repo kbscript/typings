3a7f921ee09e08e9a873a08e3cfe8d58aa92025d
KevinBarnett5506 <kevinbarnett5506@yahoo.com>
2015-04-23 16:03:32 -0700

    Create generic-pool.d.ts

---
+++ generic-pool/generic-pool.d.ts
@@ -0,0 +1,6 @@
+// Type definitions for generic-pool 2.2.0
+// Project: http://github.com/coopernurse/node-pool.git
+// Definitions by: Kevin Barnett <https://github.com/kbscript>
+// Definitions: https://github.com/kbscript/typings/generic-pool/generic-pool.d.ts
+
+///<reference path='../node/node.d.ts' />
